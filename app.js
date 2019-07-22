2626571bb6eafa01c49ff1bb47df61cb94a9c8b2
NguyênTDK <nguyentdk@vng.com.vn>
2019-07-22 14:05:21 +0700

    Removed unused route

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 
-var indexRouter = require('./routes/index')
 var apiRouter = require('./routes/api')
+var indexRouter = require('./routes/index')
 
 var app = express()
 
